b112da91681ca964c8b15059a34eb732669f6200
Calian <sevorg.nailac@gmail.com>
2026-09-23 16:39:10 -0500

    added controls for DPAD_RIGHT

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/MainTeleOp.java
@@ -9,6 +9,8 @@ import static com.seattlesolvers.solverslib.gamepad.GamepadKeys.Button.*;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.commands.intake.SetPowerIntake;
+import org.firstinspires.ftc.teamcode.subsystems.IntakeSubsystem;
 
 
 @TeleOp(name = "MainTeleOp", group = "ScrapHeads")
@@ -29,6 +31,9 @@ public class MainTeleOp extends CommandOpMode {
         assignControls();
     }
     public void  assignControls(){
+        robot.driver1.getGamepadButton(DPAD_RIGHT)
+                .whenPressed(new SetPowerIntake(robot.intake, IntakeSubsystem.INTAKE_POWER))
+                .whenReleased(new SetPowerIntake(robot.intake, IntakeSubsystem.STOP_POWER));
         
     }
 }
